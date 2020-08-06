0533910c5d4583f2c442b3137e7b2deead0e50b3
Raph Hennessy <raph.hennessy@gmail.com>
2020-08-06 14:28:03 +1000

    persistent user storage

--- src/wrapper.js
+++ src/wrapper.js
@@ -5,10 +5,57 @@ import './wrapper.scss';
 import Scoreboard from "./game/scoreboard/scoreboard";
 import Chat from "./game/chat/chat";
 
-const socket = socketIOClient("http://192.168.0.102:3000");
+const API_ENDPOINT = "http://192.168.0.102:3000";
 
-socket.on("init", (data) => {
-  console.info("init:", data);
+async function getUser(attempt = 0) {
+  const USER_KEY = "questions-user";
+
+  return new Promise((resolve) => {
+    if (attempt > 3) {
+      console.error("User setup failed too many times, giving up.");
+      return resolve(false);
+    }
+
+    const savedUser = localStorage.getItem(USER_KEY);
+    if (savedUser) {
+      const user = JSON.parse(savedUser);
+      const req = `/validate-token?id=${user.id}&token=${user.token}`;
+      fetch(API_ENDPOINT + req, {method: "GET"}).then((res) => {
+        res.json().then((res) => {
+          console.info("Validation:", res);
+          if (res.valid) {
+            return resolve(user);
+          }
+          console.warn("Cached user was invalid, attempting to create a new account");
+          localStorage.removeItem(USER_KEY);
+          return getUser(attempt++);
+        });
+      });
+    } else {
+      fetch(API_ENDPOINT + "/user", {method: "POST"}).then((res) => {
+        res.json().then((user) => {
+          localStorage.setItem(USER_KEY, JSON.stringify(user));
+          resolve(user);
+        });
+      });
+    }
+  });
+}
+
+getUser().then((user) => {
+  console.info("User:", user);
+
+  const socket = socketIOClient(API_ENDPOINT, {
+    transports: ["websocket"],
+    query: {
+      id: user.id,
+      token: user.token
+    }
+  });
+
+  socket.on("init", (data) => {
+    console.info("Init:", data);
+  });
 });
 
 function SidebarButton(props) {
