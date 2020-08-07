17dc380d54f08f59ada789f39c715f99183a0a58
Raph Hennessy <raph.hennessy@gmail.com>
2020-08-07 17:19:35 +1000

    Added a loading screen

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import Wrapper from './game/wrapper';
 import Signup from "./user/signup";
 import {getUser} from "./api";
+import LoadingScreen from "./loading";
+import './index.scss';
+
+function WrapperContainer(props) {
+  return (
+    <div className={"wrapper-container " + (props.visible ? "visible" : "hidden")}>
+      {props.children}
+    </div>
+  );
+}
 
 class QuestionsGame extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      canRender: false,
       user: null
-    }
+    };
   }
 
   componentDidMount() {
+    setTimeout(function() {
+      this.setState({
+        canRender: true
+      });
+    }.bind(this), 1000);
     getUser().then((user) => {
       this.setState({
         user: user
@@ -23,21 +38,20 @@ class QuestionsGame extends React.Component {
   }
 
   render() {
-    if (this.state.user === null) {
-      return (
-        <div className="loading-center">
-          Loading...
-        </div>
-      );
-    } else if (!this.state.user.name) {
-      return (
-        <Signup user={this.state.user} />
-      );
-    } else {
-      return (
-        <Wrapper user={this.state.user} />
-      );
-    }
+    let canRender = this.state.canRender && this.state.user;
+    return (
+      <div id="app-wrapper">
+        <WrapperContainer visible={!this.state.user}>
+          <LoadingScreen />
+        </WrapperContainer>
+        <WrapperContainer visible={canRender && !this.state.user.name}>
+          <Signup user={this.state.user} />
+        </WrapperContainer>
+        <WrapperContainer visible={canRender && this.state.user.name}>
+          <Wrapper user={this.state.user} />
+        </WrapperContainer>
+      </div>
+    );
   }
 }
 
